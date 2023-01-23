59335f928410c19ff2f85a819a2417c3faa583f7
FelipeCastro2021 <felipe2021castro@gmail.com>
2023-01-23 00:29:13 -0300

    Adicionando imagem do ReadME

--- script.js
+++ script.js
@@ -28,6 +28,26 @@ inputs.forEach(input => input.click()) */
 
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button");
+
+function addHabit() {
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5);
+  const dateExists = nlwSetup.dayExists(today);
+
+  if (dateExists) {
+    alert("Dia já incluso");
+    return
+  } 
+
+  nlwSetup.addDay(today);
+}
+
+function saveHabit() {
+  localStorage.setItem('NLWSetup@Habits', JSON.stringify(nlwSetup.data))
+}
+
+button.addEventListener("click", addHabit)
+form.addEventListener("change", saveHabit);
 
 /* nlwSetup.setData({
   run: ["18-01", "19-01", "20-01"],
@@ -35,13 +55,24 @@ const nlwSetup = new NLWSetup(form)
   food: ["18-01", "19-01", "20-01"]
 }) */
 
-const data = {
+/* const data = {
   run: ["18-01", "19-01", "20-01"],
   water: ["19-01", "20-01"],
   food: ["18-01", "19-01"],
   book: ["20-01", "22-01"],
-  programming: ["18-01", "19-01", "20-01", "21-01", "22-01"]
+  programming: [
+    "18-01",
+    "19-01",
+    "20-01",
+    "21-01",
+    "22-01",
+    "23-01",
+    "24-01",
+    "25-01"
+  ]
 }
+*/
 
+const data = JSON.parse(localStorage.getItem('NLWSetup@Habits')) || {}
 nlwSetup.setData(data)
-nlwSetup.load()
+nlwSetup.load() 
